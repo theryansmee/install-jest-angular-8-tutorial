478e8067bf35aa018d9e0705284012936eb9b236
Ryan Smee <rs@neoftl.com>
2020-06-13 20:34:47 +0100

    Added missing import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		NewComponentComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
